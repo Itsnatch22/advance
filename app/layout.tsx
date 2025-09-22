d939b9a625ba28719eb97d5b85ad7d57891126b4
Itsnatch <kamaumark996@gmail.com>
2025-09-22 20:25:58 +0300

    prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import NoahChat from "@/components/Noah";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +31,6 @@ export default function RootLayout({
       >
         <Navbar/>
         {children}
-        <NoahChat/>
         <Footer/>
       </body>
     </html>
