075eb38226738d70d36485d97bc74fea41c56089
Itsnatch <kamaumark996@gmail.com>
2025-10-03 20:57:05 +0300

    image

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -20,21 +20,22 @@ export default function Hero() {
         );
     }, []);
     return(
-        <section className="min-h-screen flex items-center bg-gradient-to-r from-green-400 via-green-300 to-white px-6 lg:px-20 dark:bg-gray-900 dark:text-white ">
+        <section className="relative h-screen flex items-center justify-center text-white bg-[url('/homepage/background.jpg')] bg-cover bg-center">
+            <div className="absolute inset-0 bg-gradient-to-r from-green-700/90 to-transparent"></div>
             <div
             className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center w-full max-w-7xl mx-auto">
                 <div
                 ref={textRef}
-                className="space-y-6">
+                className="space-y-6 relative">
                     <motion.h1
                     initial={{ opacity: 0, x: -50 }}
                     animate={{ opacity: 1, x: 0}}
                     transition={{ duration: 0.8 }}
                     className="text-4xl lg:text-6xl font-bold text-black leading-tight font-serif">
-                        Access Your <span className="text-green-700">Earned Wages</span> Before PayDay
+                        Access Your <span className="text-white">Earned Wages</span> Before PayDay
                     </motion.h1>
                     <motion.p
-                    className="max-w-lg"
+                    className="max-w-lg text-lg"
                     initial={{ opacity: 0, y: 30}}
                     animate={{ opacity: 1, y: 0}}
                     transition={{ duration: 1, delay: 0.3 }}>
@@ -54,26 +55,12 @@ export default function Hero() {
                             <BiChevronRight className="w-5 h-5 "/>
                         </Link>
                         <Link href="/employers"
-                        className="bg-white border-green-500 border px-6 py-3 rounded-xl text-lg font-medium hover:bg-green-50 transition text-green-700"
+                        className="bg-transparent border-green-500 border px-6 py-3 rounded-xl text-lg font-medium hover:bg-green-50 transition text-white hover:text-green-700"
                         >
                             For Employers
                         </Link>
                     </motion.div>
                 </div>
-
-                <motion.div
-                initial={{ opacity: 0, scale: 0.9}}
-                animate={{ opacity: 1, scale: 1}}
-                transition={{ duration: 1, delay: 0.5}}
-                className="flex justify-center">
-                    <Image
-                    src="/hero-vector.png"
-                    alt="business"
-                    width={500}
-                    height={500} 
-                    className="w-full max-w-md lg:max-w-lg"
-                    />
-                </motion.div>
             </div>
         </section>
     )
