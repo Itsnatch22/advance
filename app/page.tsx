2c2dbe7850898beabb13aa9267dc8cb6e104a0f4
Itsnatch <kamaumark996@gmail.com>
2025-10-14 19:42:53 +0300

    uugh

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
-"use client"
 import Hero from "@/components/Homepage";
 import Models from "@/components/Models";
 import Works from "@/components/Works";
 import Needs from "@/components/Needs";
 import CTA from "@/components/CTA";
-import gsap from "gsap";
 import ProblemStatement from "@/components/ProblemStatement";
 import StatsCounter from "@/components/StatsCounter";
-
-gsap.registerPlugin(ScrollTrigger);
 
 export default function App() {
 
