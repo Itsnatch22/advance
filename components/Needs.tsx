e782aee8558c6eff944d7b10c692037674cef34e
Itsnatch <kamaumark996@gmail.com>
2025-10-15 19:10:56 +0300

    calc

--- components/Needs.tsx
+++ components/Needs.tsx
@@ -1,7 +1,5 @@
 'use client'
 import { motion } from "framer-motion"
-import { BiTimer,BiShield,BiUser } from "react-icons/bi";
-import { Lock, ChartBar, Phone } from "lucide-react";
 import Image from "next/image"
 import gsap from "gsap";
 import { useEffect, useRef } from "react";
