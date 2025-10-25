b05b5a0ba39322be528b729d16e22e0246e73812
Itsnatch <kamaumark996@gmail.com>
2025-10-25 09:50:22 +0300

    file

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -4,7 +4,8 @@ import { motion } from "framer-motion"
 import { Shield, Eye, Timer, CogIcon, CircleDollarSign }  from "lucide-react"
 
 const pricing = [
-    { icon: Shield,
+    { 
+      icon: Shield,
       title: "0% Interest Charge",
       desc: "Only a nominal transaction fee applies",
     },
@@ -45,7 +46,7 @@ export default function Pricing () {
             
 
             <div className="mt-12 grid grid-cols-3 gap-6 place-items-center">
-                {pricing.slice(0,3).map((item,i) => (
+                {pricing.map((item,i) => (
                     <motion.div
                     key={i}
                     whileHover={{scale: 1.03}}
@@ -63,24 +64,7 @@ export default function Pricing () {
                 ))}
             </div>
 
-            <div className="mt-8 grid sm:grid-cols-2 gap-6 justify-center place-items-center">
-                {pricing.slice(3).map((item, i) => (
-                    <motion.div
-                    key={i}
-                    whileHover={{ scale: 1.03 }}
-                    transition={{ type: "spring", stiffness: 200 }}
-                    className="w-full max-w-sm bg-white dark:bg-neutral-900 rounded-2xl shadow-sm border border-gray-100 dark:border-neutral-800 p-6"
-                    >
-                    <item.icon className="w-10 h-10 mx-auto text-green-600 dark:text-green-400 mb-4" />
-                    <h3 className="font-semibold text-xl text-gray-900 dark:text-white">
-                        {item.title}
-                    </h3>
-                    <p className="text-gray-500 dark:text-gray-400 mt-2 text-sm">
-                        {item.desc}
-                    </p>
-                    </motion.div>
-                ))}
-            </div>
+            
             <motion.button
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.97 }}
